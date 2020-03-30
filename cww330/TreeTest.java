b60940513b9d5ebe3c4083373660e6ac5e04f870
Sunshine-creator <941389138@163.com>
2020-03-30 21:34:27 +0800

    二叉树练习

---
+++ cww330/TreeTest.java
@@ -0,0 +1,84 @@
+package cww330;
+
+public class TreeTest {
+    static class Node{
+        public  String val;
+        public  Node left;
+        public Node  right;
+        public Node(String val) {
+            this.val = val;
+        }
+    }
+    public static  Node build(){      //手动构造一棵树
+        Node a = new Node("A");
+        Node b = new Node("B");
+        Node c = new Node("C");
+        Node d = new Node("D");
+        Node e = new Node("E");
+        Node f = new Node("F");
+        Node g = new Node("G");
+        Node h = new Node("H");
+        a.left =b;
+        a.right=c;
+        b.left =d;
+        b.right = e;
+        e.left= g;
+        g.left =h;
+        c.right = f;
+        return a;
+    }
+    public static void preOrder(Node root){
+        //前序遍历
+        if(root==null){
+            return;
+        }
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);        //递归
+    }
+    public static void inOrder(Node root){   //中序遍历
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+    public static void postOrder(Node root){     //后序遍历
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+    public static int size(Node root){
+        if(root==null){
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);  //递归
+    }
+    public static int leafSize(Node root){
+        if(root==null){
+            return  0;
+        }
+        if(root.right==null && root.left==null){
+            return  1;
+        }
+        return leafSize(root.right) + leafSize(root.left);      //递归求叶子结点的个数
+    }
+    public static void main(String[] args) {
+        Node root = build();
+        System.out.print("先序遍历：");
+        preOrder(root);
+        System.out.println();
+        System.out.print("中序遍历：");
+        inOrder(root);         System.out.println("后序遍历：");
+        postOrder(root );
+        int res =  size(root);
+        System.out.println(res);
+        System.out.println();
+        int x = leafSize(root);
+        System.out.println(x);
+    }
+}
