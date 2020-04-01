d0a7e7d41b6c0dd2224a79d37478f59c942d0880
Sunshine-creator <941389138@163.com>
2020-04-01 19:57:21 +0800

    练习

--- cww330/TreeTest.java
+++ cww330/TreeTest.java
@@ -67,6 +67,28 @@ public class TreeTest {
         }
         return leafSize(root.right) + leafSize(root.left);      //递归求叶子结点的个数
     }
+   public static Node find(Node root,char toFind){   //查找元素是否存在
+        if(root==null){
+            return null;
+        }
+        if(root.val== toFind){
+            return root;
+        }
+        Node result = find(root.left,toFind);
+        if(result!=null){
+            return result;
+        }
+        return find(root.right,toFind);
+    }
+    public static int kLevelSize(Node root,int k){          //递归求第k层的节点个数
+        if(k<1||root==null){
+            return 0;
+        }
+        if(k==1){
+            return 1;
+        }
+        return kLevelSize(root.right,k-1)+kLevelSize(root.left,k-1);
+    }
     public static void main(String[] args) {
         Node root = build();
         System.out.print("先序遍历：");
